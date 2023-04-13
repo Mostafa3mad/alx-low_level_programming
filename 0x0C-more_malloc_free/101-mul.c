bd1bd83e6dc8146b1eb7206a0f03a940f565b925
Mostafa3mad <mostafa.3mad.salah@gmail.com>
2023-04-13 09:14:22 -0700

    task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,11 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+
+[200~int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 /**
  * _memset - fills memory with a constant byte
