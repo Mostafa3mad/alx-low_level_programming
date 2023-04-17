4983bec4dc26b475c0ed884dc5434ae9cf156b2a
Mostafa3mad <mostafa.3mad.salah@gmail.com>
2023-04-17 09:02:36 -0700

    task dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-t 
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
@@ -16,15 +15,4 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /*DOG_H*/
+#endif
