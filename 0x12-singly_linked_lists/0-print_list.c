7822b49576c9b9cfdf5fb63fe106f73a4983bf70
Mostafa3mad <mostafa.3mad.salah@gmail.com>
2023-04-28 14:38:17 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -8,21 +9,18 @@
  */
 
 size_t print_list(const list_t *h)
-{
-	size_t num = 0;
+ {
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
+		if (!h->str)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		num++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
-	return (num);
-}
+
+	return (s);
+ }
